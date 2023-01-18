d315cda8c8d9ba18980ac3476b117dbb6d057bee
Durban86 <durban86@verizon.net>
2023-01-18 16:53:28 -0500

    Update README.md and add score counter to script.js

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# rock-paper-scissors
+# The Odin Project - rock-paper-scissors
+https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 
 1/18/23 - Creating a Rock, Paper, Scissors game to be played in the console using JavaScript.
+
+The game should end when the player or computer reaches 5 wins - struggling with this portion
 
 Rock beats Scissors
 Paper beats Rock
--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-console.log("Hello World!");
-
-// Ask User to choose either Rock, Paper, or Scissors
-// Make sure Users choice is converted into lowercase
+//Pseudocode:
+// Ask Player to choose either Rock, Paper, or Scissors
+// Make sure Users choice isn't case sensitive
 // Ask Computer to randomly choose a number
 // Based on the number chosen, assign that number to Rock, Paper, or Scissors
-// Compare User choice to Computer choise and determine winner
-// If User chooses Rock and Computer chooses Paper, Computer wins
-// If User chooses Paper and Computer chooses Scissors, Computer wins
-// If User chooses Scissors and Computer chooses, Rock, Computer wins
-// If User chooses Rock and Computer chooses Scissors, User wins
-// If User chooses Scissors and Computer chooses Paper, User wins
-// If User chooses Paper and Computer chooses Rock, User wins
-// Create a running tally of wins for both User and Computer, the first to 5 wins
+// Compare Player choice to Computer choice and determine winner
+// If Player and Computer both choose the same thing, it's a tie
+// If Player chooses Rock and Computer chooses Paper, Computer wins
+// If Player chooses Paper and Computer chooses Scissors, Computer wins
+// If Player chooses Scissors and Computer chooses, Rock, Computer wins
+// If Player chooses Rock and Computer chooses Scissors, Player wins
+// If Player chooses Scissors and Computer chooses Paper, Player wins
+// If Player chooses Paper and Computer chooses Rock, Player wins
+// Create a running tally of wins for both Player and Computer, the first to 5 wins
 
 let playerSelection = prompt("Please type Rock, Paper, or Scissors").toLowerCase();
 
@@ -33,24 +33,41 @@ function getComputerSelection() {
 
 console.log("CPU: " + computerSelection);
 
+let computerScore = 0;
+let playerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection == computerSelection) {
-		console.log("It's a tie!");
+		return "It's a tie!";
 	} else if (playerSelection == "rock" && computerSelection == "paper") {
-		console.log("You lose, Paper beats Rock!"); 
+		computerScore++; 
+		return "You lose, Paper beats Rock!";
 	} else if (playerSelection == "rock" && computerSelection == "scissors") {
-		console.log("You win, Rock beats Scissors!");
+		playerScore++;
+		return "You win, Rock beats Scissors!";
 	} else if (playerSelection == "paper" && computerSelection == "rock") {
-		console.log("You win, Paper beats Rock!"); 
+		playerScore++
+		return "You win, Paper beats Rock!"; 
 	} else if (playerSelection == "paper" && computerSelection == "scissors") {
-		console.log("You lose, Scissors beats Paper!"); 
+		computerScore++
+		return "You lose, Scissors beats Paper!"; 
 	} else if (playerSelection == "scissors" && computerSelection == "rock") {
-		console.log("You lose, Rock beats Scissors!");
+		computerScore++
+		return "You lose, Rock beats Scissors!";
 	} else if (playerSelection == "scissors" && computerSelection == "paper") {
-		console.log("You win, Scissors beats Paper!"); 
+		playerScore++
+		return "You win, Scissors beats Paper!"; 
 	} else {
-		console.log("There's something wrong");
+		return "There's something wrong";
 	}
+
 }
 
 console.log(playRound(playerSelection, computerSelection));
+console.log("CPU Score: " + computerScore);
+
+// function game() {
+// 	for (i = 0; i < 5; i++) {
+// 		playRound(playerSelection, computerSelection);
+// 	}
+// }
